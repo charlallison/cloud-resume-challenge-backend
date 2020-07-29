42dab773e080bcb9d622c73f9f8d69d5c40cc2d0
Charles Allison <soprinye@gmail.com>
2020-07-29 22:41:18 +0100

    fix bug with app.py

---
+++ count/app.py
@@ -0,0 +1,21 @@
+import boto3
+
+
+def lambda_handler(event, context):
+    connection = boto3.resource('dynamodb', region_name='us-east-2')
+    table = connection.Table('VisitorRecord')
+
+    record = table.get_item(Key={'PKey': '1'})
+    record.get('Item') or table.put_item(Item={'PKey': '1', 'visitorCount': 0})
+
+    item = table.get_item(Key={'PKey': '1'}).get('Item')
+
+    count = item.get('visitorCount') + 1
+    table.put_item(Item={'PKey': '1', 'visitorCount': count})
+    return {
+        'statusCode': 200,
+        'body': count,
+        'headers': {
+            'Access-Control-Allow-Origin': '*'
+        }
+    }
