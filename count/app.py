2d2de72ed1e087eabd34d3c5190a51eb50057e62
Charles Allison <soprinye@gmail.com>
2020-07-29 19:51:28 +0100

    - Rewrite test using pytest. - update file name and structure - create samconfig.toml for use by SAM CLI

--- count/app.py
+++ count/app.py
@@ -1,26 +1,18 @@
 import boto3
-from botocore.exceptions import ClientError
 
 
-def lambda_handler(event, **context):
-    dynamodb = context.get('dynamodb')
-    client = dynamodb or boto3.resource('dynamodb')
-    table = client.Table('VisitorsRecord')
+def lambda_handler(event, context):
+    connection = boto3.resource('dynamodb', region_name='us-east-2')
+    table = connection.Table('VisitorsRecord')
 
-    try:
-        record = table.get_item(Key={'PKey': '1'})
-        record.get('Item') or table.put_item(Item={'PKey': '1', 'visitorCount': 0})
+    record = table.get_item(Key={'PKey': '1'})
+    record.get('Item') or table.put_item(Item={'PKey': '1', 'visitorCount': 0})
 
-        item = table.get_item(Key={'PKey': '1'}).get('Item')
+    item = table.get_item(Key={'PKey': '1'}).get('Item')
 
-        updated_record = item.get('visitorCount') + 1
-        table.put_item(Item={'PKey': '1', 'visitorCount': updated_record})
-        return {
-            'statusCode': 200,
-            'body': updated_record
-        }
-    except ClientError as e:
-        pass
+    count = item.get('visitorCount') + 1
+    table.put_item(Item={'PKey': '1', 'visitorCount': count})
     return {
-        'statusCode': 500
+        'statusCode': 200,
+        'body': count
     }
--- tests/app.py
+++ tests/app.py
@@ -1,18 +1,10 @@
-import pytest
-from boto3 import resource
 from moto import mock_dynamodb2
-
-from count import app
-
-
-@pytest.fixture()
-def api_gateway_event():
-    return {}
 
 
 @mock_dynamodb2
-def test_create_table():
-    connection = resource('dynamodb', region_name='us-east-1')
+def test_lambda_handler():
+    import boto3
+    connection = boto3.resource('dynamodb', region_name='us-east-2')
     table = connection.create_table(
         TableName='VisitorsRecord',
         KeySchema=[
@@ -33,6 +25,8 @@ def test_create_table():
         }
     )
 
-    response = app.lambda_handler(api_gateway_event, dynamodb=connection)
-    assert 0 != response['body']
+    table.put_item(Item={'PKey': '1', 'visitorCount': 1})
+    import app
+    response = app.lambda_handler(None, None)
+    assert 2 == response['body']
     assert response['statusCode'] == 200
